bc7c0636f37b8365a0e86f7bd9f17e3330de7ea8
Pfeiffer <werner.pfeiffer.extern@bertelsmann.de>
2025-06-25 14:24:10 +0200

    deleted subfolder

--- REFRESH/REFRESH/JOB_upload_by_ASEHOSTLIST_FILE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LIST="$1"
-FILE="$2"
-
-./BATCH_prepareREFRESH_file_by_ASE_HOST_FILE.sh  $LIST $FILE 2>wp1
-./BATCH_scopyREFRESH_ASEHOSTLIST_FILE.sh         $LIST $FILE 2>wp2
-./BATCH_postpareREFRESH_file_by_ASE_HOST_FILE.sh $LIST $FILE 2>wp3
-
-exit 0
--- REFRESH/REFRESH/downloadREFRESH_file_by_ASE_HOST_FILE.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ASE="$1"
-HOST="$2"
-FILENAME="$3"
-
-scp ${HOST}:/WORK/DUMP/${ASE}/wp/REFRESH/${FILENAME} ${FILENAME}
-
-exit 0
