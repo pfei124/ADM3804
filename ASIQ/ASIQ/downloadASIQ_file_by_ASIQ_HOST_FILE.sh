c9f1f0339401d25409e9251ebd5736ee583512a1
Pfeiffer <werner.pfeiffer.extern@bertelsmann.de>
2025-06-26 08:15:28 +0200

    temporarily removed

--- ASIQ/ASIQ/downloadASIQ_file_by_ASIQ_HOST_FILE.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ASIQ="$1"
-HOST="$2"
-FILENAME="$3"
-
-scp ${HOST}:/WORK/DUMP/${ASIQ}/wp/ASIQ/${FILENAME} ${FILENAME}
-
-exit 0
--- ASIQ/ASIQ/postpareASIQ_file_by_ASIQ_HOST_FILE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ASIQ="$1"
-HOST="$2"
-FILENAME="$3"
-
-
-ssh -n ${HOST} "echo 'z' ; . /DBA/nest/senv/senv.sh s $ASIQ ; cd /WORK/DUMP/${ASIQ}/wp/ASIQ ; sudo chown sybase $FILENAME ; sudo chmod 775 $FILENAME"
-
-exit 0
